73061da4256421a9dd87efe407cd3d4d84d90be6
Milind Nair <milindnair261003@gmail.com>
2023-10-22 04:47:45 +0530

    fix:minor change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /mozjpeg/,
+        use: 'null-loader',
+      });
+  
+      return config;
+    },
+  };
